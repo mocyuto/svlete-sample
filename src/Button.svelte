90234db9eacfe5488d9a3507e55de6ef35403f01
Yuto Suzuki <yuutoo.advance@gmail.com>
2022-02-24 01:42:59 +0900

    initial commit

---
+++ src/Button.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	import { name } from "./store";
+
+	const dispatch = createEventDispatcher();
+
+	let text: string;
+	name.subscribe((value) => {
+		text = value;
+	});
+
+	function update() {
+		dispatch("message", {
+			text: text,
+		});
+	}
+</script>
+
+<button on:click={update}> update! </button>
---
+++ src/Input.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { name } from "./store";
+  let text: string;
+  name.subscribe((value) => {
+    text = value;
+  });
+  $: name.set(text);
+</script>
+
+<input bind:value={text} />
